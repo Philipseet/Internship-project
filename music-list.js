7a2feaa0fd8d74fd96f9358ba76c90c164717090
Philipseet <131285357+Philipseet@users.noreply.github.com>
2023-11-28 08:40:29 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,41 @@
+
+
+let allMusic = [
+    {
+      name: "Hanuman Chalisa - T-series",
+      artist: "Gulshan Kumar",
+      img: "music-1",
+      src: "music-1"
+    },
+    {
+      name: "Super",
+      artist: "Seventeen",
+      img: "music-2",
+      src: "music-2"
+    },
+    {
+      name: "Shinu no ga ee wa",
+      artist: "Fuzi Kaze",
+      img: "music-3",
+      src: "music-3"
+    },
+    {
+      name: "Closer",
+      artist: "The Chainsmokers",
+      img: "music-4",
+      src: "music-4"
+    },
+    {
+      name: "Starboy",
+      artist: "The Weeknd",
+      img: "music-5",
+      src: "music-5"
+    },
+    {
+      name: "Run-BTS",
+      artist: "BTS",
+      img: "music-6",
+      src: "music-6"
+    },
+    
+  ];
